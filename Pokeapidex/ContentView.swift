185737001bcf54d7bfdff729c4e0edc5115d59a9
Tim Gunnarsson <tim.awg@outlook.com>
2024-07-21 17:15:01 +0200

    Initial Commit

---
+++ Pokeapidex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokeapidex
+//
+//  Created by Tim Gunnarsson on 2024-07-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokeapidex/PokeapidexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeapidexApp.swift
+//  Pokeapidex
+//
+//  Created by Tim Gunnarsson on 2024-07-21.
+//
+
+import SwiftUI
+
+@main
+struct PokeapidexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
